1db9640e6a41b74ce58b3bab31a65e32ae05cd96
koinu <daniel_ortizpary@hotmail.com>
2022-05-07 03:11:59 -0500

    version 1.1

--- img_recolector.py
+++ img_recolector.py
@@ -3,7 +3,7 @@ import requests
 import os 
 import colorama
 import logging
-from urllib import response, request
+from urllib import request
 from bs4 import BeautifulSoup as bs, ResultSet
 
 logging.basicConfig(level=logging.DEBUG,format= '%(message)s')
@@ -13,23 +13,32 @@ logging.basicConfig(level=logging.DEBUG,format= '%(message)s')
 #
 
 def main():
-    site='https://tapas.io/episode/2523566' 
+    site=getSite()
     soup=getSoup(site)
     capTitle=getTitle(soup)
     folderName=createFolder(capTitle)
     download(folderName,capTitle,soup)
     openFolder(folderName)
 
+def getSite():
+    params("Insert URL (www.tapas.io): ")
+    site= getInput(1,27)
+    if "tapas.io" in site:
+        debug("Site URL >>> "+ site)
+        return site
+    error(site+" is not a permited site")
+
 def getSoup(site:str):
+    print(colorama.Fore.MAGENTA)
     head = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0'}
     response= requests.get(url=site, headers=head)
-    print(response.status_code)
+    warm("Estatus Code RESPONSE: "+str(response.status_code))
     return bs(response.text,'html.parser')
 
 def createFolder(titleName):
-    warm("Analizando directorios existentes")
+    warm("Scanning existing directories")
     dir, subdirs, archivos = next(os.walk('.'))
-    debug("valid directories scanned \n"+str(subdirs))
+    debug("valid directories founds \n"+str(subdirs))
     dirArray=[]
     for subdir in subdirs:
         if 'Cap' in subdir:
@@ -38,7 +47,7 @@ def createFolder(titleName):
             except:
                 warm("Unvalid Dir " + subdir)
     dirArray.sort()
-    capNum=dirArray.pop()+1
+    capNum= 1 if not dirArray else dirArray.pop()+1
     folderName= "Cap "+str(capNum)+" - "+titleName
     try:
         os.mkdir(folderName)
@@ -46,7 +55,7 @@ def createFolder(titleName):
         if e.errno!=errno.EEXIST:
             raise
     finally:
-        debug("Folder create with name "+folderName)
+        params(">>>>>>>>>>>> Folder create with name "+folderName)
     return folderName
 
 
@@ -71,8 +80,7 @@ def download(folderName,titleName:str,soup:bs):
     pPictureName= folderName+".png"
     for link in soup.find_all('img',attrs={ "alt":titleName}):
         saveImg(link,pPictureName,path,'src')
-
-    print('Proceso Finalizado')
+    params('>>>>>>>>>>>> DONWLOAD FINISHED')
 
 def saveImg(link:ResultSet,filename,path,src):
     url=link.get(src) 
@@ -88,6 +96,7 @@ def saveImg(link:ResultSet,filename,path,src):
 def openFolder(folderName: str):
     path="./"+folderName+"/"
     realpath = os.path.realpath(path)
+    params("Images save in " +realpath)
     os.startfile(realpath)
 
 def debug(msg:str):
@@ -98,6 +107,11 @@ def debug(msg:str):
 def warm(msg:str):
     pre="WARNING - "
     logging.warning(colorama.Fore.YELLOW + pre + msg)
+    resetColor()
+
+def error(msg:str):
+    pre="ERROR - "
+    logging.error(colorama.Fore.RED+pre+msg)
     resetColor()
 
 def params(msg:str):
